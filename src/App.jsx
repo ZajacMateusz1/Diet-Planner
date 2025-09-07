54fcc201648eb2ced7bfe80fdae907a2bf9d0957
Mateusz Zajac <144244766+ZajacMateusz1@users.noreply.github.com>
2025-09-07 13:45:42 +0200

    Add FindMealModal component and add search by name

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import Header from "./components/Header.jsx";
 import Meal from "./components/Meal.jsx";
+import FindMealModal from "./components/FindMealModal.jsx";
 function App() {
   return (
     <>
       <Header />
-      <main className="flex flex-col items-center gap-4 my-4">
+      {/* <main className="flex flex-col items-center gap-4 my-4">
         <Meal>Breakfast</Meal>
         <Meal>Lunch</Meal>
         <Meal>Snack</Meal>
         <Meal>Dinner</Meal>
         <Meal>Supper</Meal>
-      </main>
+      </main> */}
+      <FindMealModal />
     </>
   );
 }
---
+++ src/components/FindMealModal.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import MEALS from "../../meals.json";
+export default function FindMealModal() {
+  const [query, setQuery] = useState("");
+  function handleSearch(userInput) {
+    setQuery(userInput);
+  }
+  return (
+    <dialog open={true} className="bg-[#383838] text-white p-4">
+      <h2>Choose your meal:</h2>
+      <input
+        type="text"
+        value={query}
+        onChange={(e) => {
+          handleSearch(e.target.value);
+        }}
+        className="bg-[#eee] p-1 text-black rounded-sm mb-2"
+      />
+      <ul>
+        {MEALS.filter((meal) => {
+          return query.toLowerCase() === ""
+            ? true
+            : meal.name.toLowerCase().includes(query.toLowerCase());
+        }).map((meal) => {
+          return (
+            <li key={meal.id}>
+              <p className="font-bold">{meal.name}</p>
+              <p className="meal-info flex gap-2 text-sm">
+                <span>kcal: {meal.kcal}</span>
+                <span className="text-blue-500">P: {meal.protein}</span>
+                <span className="text-yellow-400">F: {meal.fat}</span>
+                <span className="text-violet-400">C: {meal.carbs}</span>
+              </p>
+              <hr className="my-2" />
+            </li>
+          );
+        })}
+      </ul>
+    </dialog>
+  );
+}
